40ed8f715c50c8d95deba62f8b1c25627fe3c917
Henk Hesselink <github@anda.nl>
2024-11-25 20:58:27 -0800

    Refactor
    - move config to datastructure
    - move remote/device init into main hass object

--- appdaemon/harmony.py
+++ appdaemon/harmony.py
@@ -19,7 +19,9 @@ REMNAME = 'web_bda758'
 
 
 class RemoteControl(object):
-    def __init__(self, name, keys):
+    def __init__(self, appdaemon, name, keys):
+        self.appdaemon = appdaemon
+        self.log = appdaemon.log
         self.name = name
         self.keys = keys
         self.addresses = {}
@@ -36,13 +38,9 @@ class RemoteControl(object):
     def key(self, k):
         return self.keys.get(k)
 
-remotes = []
-
-r = RemoteControl('URC3680', urc3680_rc6_keys)
-remotes.append(r)
 
 class Device(object):
-    def tx_cmd(self, ad, cmd, **kwargs):
+    def tx_cmd(self, cmd, **kwargs):
         proto = self.proto
         if proto != 'pronto' and isinstance(cmd, str) and cmd.startswith('0000 '):
             # if payload looks like Pronto, send it that way
@@ -56,95 +54,106 @@ class Device(object):
             cmdfld = 'data'
         kwargs[cmdfld] = cmd
 
-        ad.call_service(svc, **kwargs)
+        self.appdaemon.call_service(svc, **kwargs)
 
 class Vizio_TV_M656G4(Device):
-    def __init__(self, name, instance=0):
+    def __init__(self, appdaemon, name, instance=0):
         self.proto = 'nec'
         self.commands = vizio_tv_m656g4_cmds
+        self.appdaemon = appdaemon
         self.name = name
         self.instance = instance
 
-    def send_cmd(self, ad, repcnt, key):
+    def send_cmd(self, repcnt, key):
         cmd = self.commands.get(key)
         if not cmd:
             return
         name, command = cmd
-        self.tx_cmd(ad, command, address=0xFB04)
-
-r.add_device(1, Vizio_TV_M656G4('Vizio TV'))
+        self.tx_cmd(command, address=0xFB04)
 
 class Apple_TV_4K(Device):
-    def __init__(self, name, instance=0):
+    def __init__(self, appdaemon, name, instance=0):
         self.proto = 'sony'
         self.commands = sony_cmds_for_atv
+        self.appdaemon = appdaemon
         self.name = name
         self.instance = instance
 
-    def send_cmd(self, ad, repcnt, key):
+    def send_cmd(self, repcnt, key):
         cmd = self.commands.get(key)
         if not cmd:
             return
         name, (data, nbits) = cmd
         # 'wait' empirically determined for urc3680...
         if repcnt:
-            self.tx_cmd(ad, data, nbits=nbits, wait=115)
+            self.tx_cmd(data, nbits=nbits, wait=115)
         else:
-            self.tx_cmd(ad, data, nbits=nbits, wait=10)
-
-r.add_device(2, Apple_TV_4K('Apple TV'))
+            self.tx_cmd(data, nbits=nbits, wait=10)
 
 class Cisco_STB_8742(Device):
-    def __init__(self, name, instance=0):
+    def __init__(self, appdaemon, name, instance=0):
         self.proto = 'pronto'
         self.commands = cisco_stb_8742_cmds
+        self.appdaemon = appdaemon
         self.name = name
         self.instance = instance
 
-    def send_cmd(self, ad, repcnt, key):
+    def send_cmd(self, repcnt, key):
         cmd = self.commands.get(key)
         if not cmd:
             return
         name, data = cmd
-        self.tx_cmd(ad, data)
-
-r.add_device(3, Cisco_STB_8742('Set Top Box'))
+        self.tx_cmd(data)
 
 class Denon_AVR_S760(Device):
-    def __init__(self, name, instance=0):
+    def __init__(self, appdaemon, name, instance=0):
         self.proto = 'panasonic'
         self.commands = denon_avr_s760_cmds
+        self.appdaemon = appdaemon
         self.name = name
         self.instance = instance
 
-    def send_cmd(self, ad, repcnt, key):
+    def send_cmd(self, repcnt, key):
         cmd = self.commands.get(key)
         if not cmd:
             return
-        self.tx_cmd(ad, cmd, address=0x2A4C)
-
-r.add_device(5, Denon_AVR_S760('Receiver'))
+        self.tx_cmd(cmd, address=0x2A4C)
 
 class Panasonic_DVD_S700(Device):
-    def __init__(self, name, instance=0):
+    def __init__(self, appdaemon, name, instance=0):
         self.proto = 'panasonic'
         self.commands = panasonic_dvd_s700_cmds
+        self.appdaemon = appdaemon
         self.name = name
         self.instance = instance
 
-    def send_cmd(self, ad, repcnt, key):
+    def send_cmd(self, repcnt, key):
         cmd = self.commands.get(key)
         if not cmd:
             return
         name, command = cmd
-        self.tx_cmd(ad, command, address=0x4004)
-
-r.add_device(7, Panasonic_DVD_S700('DVD Player'))
+        self.tx_cmd(command, address=0x4004)
 
 
 class Activity(object):
     def __init__(self):
         pass
+
+
+config = {
+    'remotes': {
+        'URC3680': {
+            'keys': urc3680_rc6_keys,
+            'devices': (
+                (1, Vizio_TV_M656G4, 'Vizio TV'),
+                (2, Apple_TV_4K, 'Apple TV'),
+                (3, Cisco_STB_8742, 'Set Top Box'),
+                (5, Denon_AVR_S760, 'Receiver'),
+                (7, Panasonic_DVD_S700, 'DVD Player'),
+            )
+        }
+    },
+}
 
 class Harmony(hass.Hass):
 
@@ -153,9 +162,19 @@ class Harmony(hass.Hass):
 
         self.last_event = None
         self.repcnt = 0
+        self.remotes = []
+
+        self.read_config()
 
         self.listen_event(self.handle_rc6_event, 'esphome.receiver_rf', device_id=REMOTE)
         self.listen_event(self.handle_rc6_event, 'esphome.receiver_ir', device_id=REMOTE)
+
+    def read_config(self):
+        for name, remconfig in config['remotes'].items():
+            remote = RemoteControl(self, name, remconfig['keys'])
+            for ix, devtype, name in remconfig['devices']:
+                remote.add_device(ix, devtype(self, name))
+            self.remotes.append(remote)
 
     def handle_rc6_event(self, event_name, data, kwargs):
         a = data.get('address')
@@ -172,7 +191,7 @@ class Harmony(hass.Hass):
             a = 5
 
         # get remote that handles this address, last added if multiple
-        v = [(r, r.get_device(a)) for r in reversed(remotes) if r.has_device(a)]
+        v = [(r, r.get_device(a)) for r in reversed(self.remotes) if r.has_device(a)]
         if not v:
             return
         remote, device = v[0]
@@ -197,4 +216,4 @@ class Harmony(hass.Hass):
             self.last_event = (a, c, m, t)
             self.repcnt = 0
 
-        device.send_cmd(self, self.repcnt, key)
+        device.send_cmd(self.repcnt, key)
